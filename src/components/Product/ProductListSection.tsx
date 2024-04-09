316b91b2de90b536db33a65798cd624a68108f2b
humbl3LilaS <sabishinekobebe@gmail.com>
2024-04-09 18:53:54 +0630

    Add: Navigation link in SearchedProductList back to Product Page

---
+++ src/components/Product/ProductListSection.tsx
@@ -0,0 +1,21 @@
+import ProductSearchBar from "@/components/Product/ProductSearchBar";
+import {useSearchParams} from "react-router-dom";
+import ProductList from "@/components/Product/ProductList.tsx";
+import SearchedProductList from "@/components/Product/SearchedProductList.tsx";
+
+export default function ProductListSection() {
+    const [searchParams] = useSearchParams();
+
+    const searchString = searchParams.get("search") as string;
+
+    console.log(searchString);
+
+    return (
+        <div className={"my-4"}>
+            <ProductSearchBar/>
+            {searchString  && <SearchedProductList searchString={searchString}/>}
+            {!searchString && <ProductList/>}
+        </div>
+    );
+}
+
--- src/components/Product/SearchedProductList.tsx
+++ src/components/Product/SearchedProductList.tsx
@@ -1,6 +1,8 @@
 import {useQuery} from "@tanstack/react-query";
 import {getProductByName} from "@/services/productApi.ts";
 import ProductListItem from "@/components/Product/ProductListItem.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {useNavigate} from "react-router-dom";
 
 type SearchedProductListProps = {
     searchString: string
@@ -11,9 +13,20 @@ export default function SearchedProductList({searchString}: SearchedProductListP
         queryFn: async () => getProductByName(searchString),
         staleTime: 1000 * 60 * 60
     });
+
+    const navigate = useNavigate()
+
+    const btnHandler = () => {
+        navigate("/products")
+    }
+
     return(
-        <ul className={"max-w-productList p-6 flex flex-col gap-y-5 rounded bg-[#eee]"}>
-            {searchQuery.data && searchQuery.data.map(ele => <ProductListItem key={ele.productCode} productData={ele}/>)}
-        </ul>
+        <>
+            <ul className={"max-w-productList p-6 flex flex-col gap-y-5 rounded bg-[#eee]"}>
+                {searchQuery.data && searchQuery.data.map(ele => <ProductListItem key={ele.productCode}
+                                                                                  productData={ele}/>)}
+            </ul>
+            <Button className={"mt-4"} onClick={btnHandler}>See all products</Button>
+        </>
     )
 }
