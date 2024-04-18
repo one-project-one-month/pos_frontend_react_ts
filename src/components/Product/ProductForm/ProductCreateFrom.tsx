dbb18ee72fe809dc34d607e4cc7e9ffc3d3acd45
Amoz19 <aungmon10co@gmail.com>
2024-04-18 12:41:43 +0630

    Fix submit button

--- src/components/Product/ProductForm/ProductCreateFrom.tsx
+++ src/components/Product/ProductForm/ProductCreateFrom.tsx
@@ -1,30 +1,29 @@
-import {useLocation, useNavigate} from "react-router-dom";
-import {SubmitHandler, useForm} from "react-hook-form";
-import {Inputs} from "@/type/formSchema.ts";
+import { useLocation, useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Inputs } from "@/type/formSchema.ts";
 import useRenderForm from "@/hook/useRenderForm.tsx";
-import {productFormConst} from "@/constants/form-constate.ts";
-import SubmitButton from "@/components/ui/submit-button.tsx";
+import { productFormConst } from "@/constants/form-constate.ts";
 
-import {useCreateProduct} from "@/services/mutation.ts";
+
+import { useCreateProduct } from "@/services/mutation.ts";
 
 export default function ProductCreateFrom() {
     const navigate = useNavigate();
-    const {register, handleSubmit, formState: {errors}} = useForm<Inputs>();
-    const formElements = useRenderForm({formconst: productFormConst, errors, register});
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const formElements = useRenderForm({ formconst: productFormConst, errors, register, title: "Product" });
     // const { mutate } = useCreateNew("product");
-    const {state} = useLocation();
+    const { state } = useLocation();
 
-     const {mutate} = useCreateProduct(state);
+    const { mutate } = useCreateProduct(state);
     const onSubmit: SubmitHandler<Inputs> = (data) => {
         mutate(data);
-        navigate(`..`, {relative: "path"});
+        navigate(`..`, { relative: "path" });
     };
 
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="w-4/6 m-auto">
             {formElements}
-            <SubmitButton/>
         </form>
     );
 }
