f209df70fc63f291db08e41b13d8e821071ec599
Amoz19 <aungmon10co@gmail.com>
2024-04-21 16:03:27 +0630

    Resolve merged

--- src/Page/SaleInvoice/SaleInvoice.tsx
+++ src/Page/SaleInvoice/SaleInvoice.tsx
@@ -8,74 +8,33 @@ import { useBillingCartStore } from "@/store/billingCartStore"
 import { useCreateNew } from "@/hook/management/useAddQuery"
 import { useNavigate } from "react-router-dom"
 import { useToast } from "@/components/ui/use-toast"
-  
-  type InputProps = {
-    label: Path<TInvoiceFormValues>
-    register: UseFormRegister<TInvoiceFormValues>,
-    errors: FieldErrors<TInvoiceFormValues>,
-    datas?: (TCustomer | TStaff)[] | undefined,
-    required: boolean,
-  }
-  
-  const Input = ({ label, register,errors }: InputProps) =>  {
 
-    const isNumberInput = label === "paymentAmount" || label === "receiveAmount";
+type InputProps = {
+  label: Path<TInvoiceFormValues>
+  register: UseFormRegister<TInvoiceFormValues>,
+  errors: FieldErrors<TInvoiceFormValues>,
+  datas?: (TCustomer | TStaff)[] | undefined,
+  required: boolean,
+}
 
-    return (
-      <div className="flex my-2 items-center justify-between">
-        <label className="mb-1 capitalize">{label}</label>
-        <div className="flex flex-col">
-          <input 
-          {...register(label, { 
-            required: `${label} is required`, 
-            ...(isNumberInput && { 
+const Input = ({ label, register, errors }: InputProps) => {
+
+  const isNumberInput = label === "paymentAmount" || label === "receiveAmount";
+
+  return (
+    <div className="flex my-2 items-center justify-between">
+      <label className="mb-1 capitalize">{label}</label>
+      <div className="flex flex-col">
+        <input
+          {...register(label, {
+            required: `${label} is required`,
+            ...(isNumberInput && {
               pattern: {
                 value: /^[0-9]*$/,
                 message: "Please enter a number value only"
-              } 
-            }) 
-          })} type={isNumberInput ? 'number' : 'text'} step={isNumberInput ? '1' : undefined} placeholder={`Enter ${label}`} className="w-64 border border-gray-300 rounded-lg p-2" />  
-          {errors?.[label] ? <p className="text-red-400">{errors?.[label]?.message}</p> : null}
-        </div>
-      </div>
-    )
-  }
-
-  const Select = ({ label, register, errors, datas }: InputProps) => (
-      <div className="flex my-2 items-center justify-between">
-        <label className="mb-1 capitalize">{label}</label>
-        <div className="flex flex-col">
-          <select {...register(label, { required: `${label} is required` })} className="w-64 border border-gray-300 rounded-lg p-2 bg-white">
-            <option value="" hidden>{`Select ${label}`}</option>
-            {label === "paymentType" ? (
-              ['Cash', 'Mobile Payment'].map((data) => (
-                <option key={data} value={data}>
-                  {data}
-                </option>
-              ))
-            ) : (
-              datas?.map((data) => {
-                let name = ""
-                switch(label){
-                  case 'customers' : 
-                    const customer = data as TCustomer;
-                    name = customer.customerName;
-                    break;
-                  case 'staffs' : 
-                    const staff = data as TStaff;
-                    name = staff.staffName;
-                    break;
-                  default: 
-                    break;
-                }
-                return (
-                  <option key={data?.id} value={name}>
-                    {name}
-                  </option>
-                );
-              })
-            )}
-          </select>
+              }
+            })
+          })} type={isNumberInput ? 'number' : 'text'} step={isNumberInput ? '1' : undefined} placeholder={`Enter ${label}`} className="w-64 border border-gray-300 rounded-lg p-2" />
         {errors?.[label] ? <p className="text-red-400">{errors?.[label]?.message}</p> : null}
       </div>
     </div>
@@ -84,28 +43,45 @@ import { useToast } from "@/components/ui/use-toast"
 
 const Select = ({ label, register, errors, datas }: InputProps) => (
   <div className="flex my-2 items-center justify-between">
-    <label className="mb-1">{label}</label>
+    <label className="mb-1 capitalize">{label}</label>
     <div className="flex flex-col">
       <select {...register(label, { required: `${label} is required` })} className="w-64 border border-gray-300 rounded-lg p-2 bg-white">
         <option value="" hidden>{`Select ${label}`}</option>
-        {label === "Payment Type" ? (
+        {label === "paymentType" ? (
           ['Cash', 'Mobile Payment'].map((data) => (
             <option key={data} value={data}>
               {data}
             </option>
           ))
         ) : (
-          datas?.map((data) => (
-            <option key={data.id} value={data.id}>
-              {label === "Customers" ? (data as TCustomer).customerName : label === "Staffs" ? (data as TStaff).staffName : null}
-            </option>
-          ))
+          datas?.map((data) => {
+            let name = ""
+            switch (label) {
+              case 'customers':
+                const customer = data as TCustomer;
+                name = customer.customerName;
+                break;
+              case 'staffs':
+                const staff = data as TStaff;
+                name = staff.staffName;
+                break;
+              default:
+                break;
+            }
+            return (
+              <option key={data?.id} value={name}>
+                {name}
+              </option>
+            );
+          })
         )}
       </select>
       {errors?.[label] ? <p className="text-red-400">{errors?.[label]?.message}</p> : null}
     </div>
   </div>
 )
+
+
 
 const InvoiceItem: React.FC<TInvoiceItemProps> = ({ label, value }) => (
   <div className="flex justify-between">
@@ -114,15 +90,15 @@ const InvoiceItem: React.FC<TInvoiceItemProps> = ({ label, value }) => (
   </div>
 );
 
-  const SaleInvoice = () => {
-    const { register, handleSubmit, formState: { errors }} = useForm<TInvoiceFormValues>()
-    const ref = useRef<HTMLFormElement>(null);
-    const navigate = useNavigate();
-    const {toast} = useToast();
-    const { data: customers } = useCustomQuery<TCustomer[]>(
-      "customers",
-      () => queryFn("customers"),
-      0,
+const SaleInvoice = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<TInvoiceFormValues>()
+  const ref = useRef<HTMLFormElement>(null);
+  const navigate = useNavigate();
+  const { toast } = useToast();
+  const { data: customers } = useCustomQuery<TCustomer[]>(
+    "customers",
+    () => queryFn("customers"),
+    0,
   )
   const { data: staffs } = useCustomQuery<TStaff[]>(
     "staffs",
@@ -130,15 +106,15 @@ const InvoiceItem: React.FC<TInvoiceItemProps> = ({ label, value }) => (
     0,
   )
   const orderProducts = useBillingCartStore((state) => state.cart);
-  
-  const priceArr = orderProducts?.map((product:TProductInCart) => product?.product.price)
-  const subTotal = priceArr.reduce((acc,curr) => acc + curr, 0);
 
-  const totalAmount:number = subTotal - 50
+  const priceArr = orderProducts?.map((product: TProductInCart) => product?.product.price)
+  const subTotal = priceArr.reduce((acc, curr) => acc + curr, 0);
+
+  const totalAmount: number = subTotal - 50
   const currentDate = new Date();
 
 
-  const options: DateTimeOptions  = {
+  const options: DateTimeOptions = {
     day: '2-digit',
     month: 'short',
     year: 'numeric',
@@ -146,32 +122,33 @@ const InvoiceItem: React.FC<TInvoiceItemProps> = ({ label, value }) => (
     minute: '2-digit',
     hour12: true,
   }
-  const formattedDateTime = currentDate.toLocaleString('en-US',options).replace(/,/, '');
+  const formattedDateTime = currentDate.toLocaleString('en-US', options).replace(/,/, '');
 
-  const {mutate} = useCreateNew('saleInvoice');
-  
+  const { mutate } = useCreateNew('saleInvoice');
+
   const onSubmit: SubmitHandler<TInvoiceFormValues> = (data) => {
-    const { customers: customerCode, staffs: staffCode, paymentAmount, receiveAmount,...rest } = data;
+    const { customers: customerCode, staffs: staffCode, paymentAmount, receiveAmount, ...rest } = data;
     const parsedPaymentAmount = typeof paymentAmount === 'string' ? parseFloat(paymentAmount) : paymentAmount;
     const parsedReceiveAmount = typeof receiveAmount === 'string' ? parseFloat(receiveAmount) : receiveAmount;
     const change = paymentAmount - receiveAmount
 
     mutate({
       formData: {
-      ...rest,
-      customerCode,
-      staffCode,
-      paymentAmount: parsedPaymentAmount,
-      receiveAmount: parsedReceiveAmount,
-      totalAmount,
-      change,
-      saleInvoiceDateTime: formattedDateTime,
-      'discount': 50,
-      'tax': 0.00
-  }, route: 'saleInvoice'})
-  
+        ...rest,
+        customerCode,
+        staffCode,
+        paymentAmount: parsedPaymentAmount,
+        receiveAmount: parsedReceiveAmount,
+        totalAmount,
+        change,
+        saleInvoiceDateTime: formattedDateTime,
+        'discount': 50,
+        'tax': 0.00
+      }, route: 'saleInvoice'
+    })
+
     navigate('/sale-invoice/history');
-    toast({description: "Success"})
+    toast({ description: "Success" })
   }
 
   const handleSave = () => {
@@ -183,24 +160,24 @@ const InvoiceItem: React.FC<TInvoiceItemProps> = ({ label, value }) => (
   return (
     <div className="w-9/12 mx-auto my-10">
 
-        <div className="flex justify-between items-center">
-            <h1 className="text-3xl">New Invoice</h1>
-            {
-              orderProducts.length !== 0 ? <button className="bg-gray-900 text-white inline-block py-2 px-4 rounded-md" onClick={handleSave}>Save</button> : <></>
-            }
-        </div>
-        <div className="mt-5 bg-gray-100 py-3 px-5 rounded-sm">
-            <form className="grid grid-cols-2 gap-x-40" ref={ref}>
-              <Input label="voucherNo" register={register} required errors={errors} />
-              <Select label="paymentType" register={register} required errors={errors} />
-              <Select label="customers" datas={customers} register={register} required errors={errors} />
-              <Input label="paymentAmount" register={register} required errors={errors} />
-              <Select label="staffs" datas={staffs} register={register} required errors={errors} />
-              <Input label="receiveAmount" register={register} required errors={errors} />  
-            </form>
-            {
-              orderProducts?.length !== 0 ? 
-              <>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl">New Invoice</h1>
+        {
+          orderProducts.length !== 0 ? <button className="bg-gray-900 text-white inline-block py-2 px-4 rounded-md" onClick={handleSave}>Save</button> : <></>
+        }
+      </div>
+      <div className="mt-5 bg-gray-100 py-3 px-5 rounded-sm">
+        <form className="grid grid-cols-2 gap-x-40" ref={ref}>
+          <Input label="voucherNo" register={register} required errors={errors} />
+          <Select label="paymentType" register={register} required errors={errors} />
+          <Select label="customers" datas={customers} register={register} required errors={errors} />
+          <Input label="paymentAmount" register={register} required errors={errors} />
+          <Select label="staffs" datas={staffs} register={register} required errors={errors} />
+          <Input label="receiveAmount" register={register} required errors={errors} />
+        </form>
+        {
+          orderProducts?.length !== 0 ?
+            <>
               <Table className="rounded-md border my-5 bg-white">
                 <TableHeader>
                   <TableRow>
@@ -212,34 +189,33 @@ const InvoiceItem: React.FC<TInvoiceItemProps> = ({ label, value }) => (
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {
-                      orderProducts?.map((product: TProductInCart,index: number) => {
-                        return <TableRow>
-                          <TableCell className="font-medium">{index + 1}</TableCell>
-                          <TableCell>{product?.product?.productName}</TableCell>
-                          <TableCell>{product?.count}</TableCell>
-                          <TableCell>{product?.product?.price}</TableCell>
-                          <TableCell className="text-right">{product?.product?.price * product?.count}  MMK</TableCell>
-                        </TableRow>
-                      }
-                      )
+                  {
+                    orderProducts?.map((product: TProductInCart, index: number) => {
+                      return <TableRow>
+                        <TableCell className="font-medium">{index + 1}</TableCell>
+                        <TableCell>{product?.product?.productName}</TableCell>
+                        <TableCell>{product?.count}</TableCell>
+                        <TableCell>{product?.product?.price}</TableCell>
+                        <TableCell className="text-right">{product?.product?.price * product?.count}  MMK</TableCell>
+                      </TableRow>
                     }
+                    )
+                  }
                 </TableBody>
               </Table>
               <div className="flex justify-end">
                 <div className="w-1/4 flex flex-col gap-2">
-                <InvoiceItem label="Subtotal" value={`${subTotal} MMK`} />
-                <InvoiceItem label="Discount" value="500 MMK" />
-                <InvoiceItem label="Tax" value="0.00" />
-                <div className="w-full h-[1px] bg-gray-300" />
-                <InvoiceItem label="Total" value={`${totalAmount} MMK`} />
+                  <InvoiceItem label="Subtotal" value={`${subTotal} MMK`} />
+                  <InvoiceItem label="Discount" value="500 MMK" />
+                  <InvoiceItem label="Tax" value="0.00" />
+                  <div className="w-full h-[1px] bg-gray-300" />
+                  <InvoiceItem label="Total" value={`${totalAmount} MMK`} />
                 </div>
               </div>
-              </>
-              : <></>
-            }
-           
-        </div>
+            </>
+            : <></>
+        }
+
       </div>
     </div>
   )
