6e3b63cf3922103647489dd3e871d2a9a842ad26
humbl3LilaS <sabishinekobebe@gmail.com>
2024-04-26 19:18:46 +0630

    Add: Skeleton to the categoryList

--- src/components/Product/ListSekeleton.tsx
+++ src/components/Product/ListSekeleton.tsx
@@ -1,11 +1,12 @@
 import {Skeleton} from "@/components/ui/skeleton.tsx";
+import {cn} from "@/lib/utils.ts";
 
-export default function ListSkeleton() {
+export default function ListSkeleton({className}: {className ?: string}) {
     console.log("list skeleton")
     return (
-        <div className={"w-full flex flex-col gap-y-8 "}>
+        <div className={cn("w-full h-[80vh] flex flex-col gap-y-8", className)}>
                 <Skeleton className={"w-2/5 h-10 "}/>
-                <Skeleton className={"w-full h-[500px] rounded-xl"}/>
+                <Skeleton className={"w-full h-2/3  rounded-xl"}/>
                 <Skeleton className={"w-2/5 h-10 self-end"}/>
         </div>
     )
