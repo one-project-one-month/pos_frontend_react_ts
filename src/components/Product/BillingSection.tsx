afacb948b58dbd70e9ab17318536dc5c0f7f76dc
humbl3LilaS <sabishinekobebe@gmail.com>
2024-04-28 21:07:55 +0630

    Fix: Billing Section UI

--- src/components/Product/BillingSection.tsx
+++ src/components/Product/BillingSection.tsx
@@ -1,15 +1,20 @@
-import Cart from "@/components/Product/Cart/Cart.tsx";
 import StaffSelection from "@/components/Product/StaffSelection.tsx";
+import {useCartStore} from "@/store/cartStore.ts";
+import CartEmpty from "@/components/Product/Cart/CartEmpty.tsx";
+import CartList from "@/components/Product/Cart/CartList.tsx";
+import {cn} from "@/lib/utils.ts";
 
 export default function BillingSection() {
 
+    const {products} = useCartStore();
+
     return (
-        <div className={"w-[50%] ml-8 p-6 bg-blue-50 rounded"}>
-            <div className={" w-full flex justify-between items-center"}>
-                <h2 className={"text-cyan-900 font-bold text-xl"}>Staff</h2>
-                <StaffSelection />
-            </div>
-            <Cart />
+        <div className={cn("w-[50%] ml-8 p-6 bg-blue-50 rounded ", products.length === 0 && "h-fit py-10 mt-[20vh]")}>
+            {products.length !== 0 ? <div className={" w-full flex justify-between items-center"}>
+              <h2 className={"text-cyan-900 font-bold text-xl"}>Staff</h2>
+              <StaffSelection/>
+            </div> : <CartEmpty/>}
+            {products.length !== 0 && <CartList/>}
         </div>
-    )
+    );
 }
